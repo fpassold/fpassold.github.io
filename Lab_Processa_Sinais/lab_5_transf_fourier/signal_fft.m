0cb5344d7a244cb514e102fe8f81881be2895b15
Fernando Passold <55355936+fpassold@users.noreply.github.com>
2023-11-02 23:17:43 -0300

    Lab Proc Sinais Trans Fourier FFT
    Lab Proc Sinais Trans Fourier FFT

--- Lab_Processa_Sinais/lab_5_transf_fourier/signal_fft.m
+++ Lab_Processa_Sinais/lab_5_transf_fourier/signal_fft.m
@@ -1,14 +1,15 @@
-function signal_fft(t,sinal,duplicar_amostras)
+function signal_fft3(t,sinal,tol)
 % signal_fft.m
 % Mostra o espectro de um sinal baseado na FFT do sinal
-% Usa função `fft()` e `fftshit()` para obter o espectro
-% Baseado em rotina original Prof. Mikhail Polonskii
+% Usa função `fft()` e `fftshift()` para obter o espectro
+%
 % Parâmetros de entrada
 %   t = vetor tempo (em segundos): posição temporal de cada amostra
-%   signal = amplitude do sinal em cada instante de tempo
-%   k_ini = primeira amostra
-%   k_fim = última amostra
-%   duplicar_amostras = 1 (duplica) ou 0 (não duplicar)
+%   sinal = amplitude do sinal em cada instante de tempo
+%   tol = tolerência (em %) usada para ainda considerar magnitudes
+%         baixas como harmônica do sinal (sugerido 0.1%)
+%
+% Fernando Passold, em 02/11/2023
 
 
 u = length(sinal);
@@ -18,23 +19,8 @@ k_fim = u;
 tt = t(k_fim) - t(k_ini);   % intervalo de tempo considerado (em segundos)
 n = k_fim - k_ini + 1;      % número de amostras
 I = sinal(k_ini:k_fim);     % separando a região de interesse
-% fprintf('signal_fft >> Intervalo de tempo considerado: [%g, %g]\n', t(k_ini), t(k_fim));
-if duplicar_amostras == 1
-    for k = 1:2*n   % duplicar número de amostras
-        if (k <= n)
-            I2(k) = I(k); % I2(k,2) = I(k);
-            t2(k) = t(k); % t2(k,1) = t(k);
-        else
-            I2(k) = I(k-n);             % I2(k,2) = I(k-n);
-            t2(k) = t(k_fim) + t(k-n);  % t2(k,1) = t(k_fim) + t(k-n);
-        end
-    end
-    n = 2*n;    % dobrou numero de amostras
-    tt = 2*tt;
-else
-    I2 = I;
-    t2 = t;
-end
+I2 = I;
+t2 = t;
 
 fprintf('signal_fft >> Número de amostras consideradas, n = %d\n', n);
 fprintf('signal_fft >> Intervalo de tempo considerado, tt = %g\n', tt);
@@ -49,26 +35,45 @@ title('Sinal analisado (domínio tempo)')
 xlabel('t (segundos)')
 grid
 
-Fnyq = n/tt;    % dobro da freq. de Nyquist
-fprintf('signal_fft >> Fnyq: %d\n', Fnyq);
-f = fft(I2);    % DFT sobre o sinal, retorna n-pontos (n=length(I2))
-F_ini = -Fnyq/2;
-F_fim = Fnyq/2-1/n;
-F_inc = Fnyq/n;
-Freq = F_ini: F_inc: F_fim;     % formar escala de frequencias
-u = length(Freq);
-fprintf('signal_fft >> Freq = [%g : %g : %g] (%g pontos)\n', F_ini, F_inc, F_fim, u);
-zz = 2*abs(fftshift(f))/n;  % desloca o componente da frequencia zero (nível DC) do sinal no centro do espectro obtido via fft()
+% Calculando a FFT
+fft_result = fft(I2);    % DFT sobre o sinal, retorna n-pontos (n=length(I2))
+
+% Fs = Taxa de amostragem (Hz)
+% Calculo das frequencias correspondentes
+% Fs = Taxa de amostragem (Hz)
+Fs = 1/(t(2)-t(1));     % assumindo amostras equidistantes entre si
+fprintf('signal_fft >> Freq. de amostragem original, Fs = %g Hz\n', Fs);
+Fnyq = Fs/2;
+freq_ini = -Fnyq;
+freq_inc = 2*Fnyq/n;
+freq_fim =  Fnyq - freq_inc;            % retrocede 1 ponto
+freq = freq_ini: freq_inc : freq_fim;   % forma vetor frequencia (em Hz)
+u = length(freq);
+fprintf('signal_fft >> Frequências: [ %g : %g : %g] Hz (%d pontos)\n', freq_ini, freq_inc, freq_fim, u);
+% Calculando a magnitude (descartando a fase)
+mag = 2*abs(fftshift(fft_result))/n;    % desloca o componente da frequencia zero (nível DC) do sinal no centro do espectro obtido via fft()
 
 subplot(2,1,2)
-plot(Freq, zz);
+plot(freq, mag, 'bo-');
 title('Sinal analisado (domínio Frequência)')
 xlabel('Frequência (Hz)')
-axis([0 F_fim 0 max(zz)*1.1])
+axis([0 freq_fim 0 max(mag)*1.1])
 grid
 
-
-
-
-
-
+% Encontrando frquencias correspondentes aos picos do sinal no dominio
+% frequencia
+% Varrendo vetor frequencia
+% freq_central = freq(u/2+1); % eventualmente rende 0 Hz
+tol2= tol/100;
+[maior, last] = max(mag);
+limiar = maior*tol2;
+disp('signal_fft >> Harmônicas detectadas:');
+fprintf('signal_fft >> [Freq\tMag]\n');
+for index = u/2+1:u
+    if mag(index)>=limiar;
+       fprintf('signal_fft >> [%g\t%g]\n', freq(index), mag(index));
+       last = index;
+    end
+end
+% re-escalonando figura
+axis([0 freq(last)*2 0 maior*1.1])
